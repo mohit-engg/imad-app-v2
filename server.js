2d9b33c3f8d12e5297ccb080f268565c09629f2a
mohit-engg <mohit4you2411@gmail.com>
2017-03-25 11:01:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile('article one will be server here');
+  res.send('article one will be server here');
 });
 app.get('/article-two', function (req, res) {
-  res.sendFile('article two will be server here');
+  res.send('article two will be server here');
 });
 app.get('/article-three', function (req, res) {
-  res.sendFile('article three will be server here');
+  res.send('article three will be server here');
 });
 
 app.get('/ui/style.css', function (req, res) {
