70a58e7fa25f31c1b4f7de2ebf564ca406fb9d41
mohit-engg <mohit4you2411@gmail.com>
2017-03-25 10:56:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.sendFile('article one will be server here');
+});
+app.get('/article-two', function (req, res) {
+  res.sendFile('article two will be server here');
+});
+app.get('/article-three', function (req, res) {
+  res.sendFile('article three will be server here');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
